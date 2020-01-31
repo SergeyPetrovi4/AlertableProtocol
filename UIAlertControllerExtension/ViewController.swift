c27560cc350763396b826695eef63ac6accc10ac
Sergey Krasiuk <sergey.krasiuk@gmail.com>
2020-01-31 16:09:40 +0200

    Initial Commit

---
+++ UIAlertControllerExtension/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  UIAlertControllerExtension
+//
+//  Created by Sergey Krasiuk on 31/01/2020.
+//  Copyright © 2020 Sergey Krasiuk. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
